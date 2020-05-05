08736e79ce6e117bf419e1dc18bf231be8cb4117
yamazakiyuu <pplmtv@gmail.com>
2020-05-05 20:37:44 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yuu/taskapp/TaskApp.kt
@@ -0,0 +1,11 @@
+package jp.techacademy.yuu.taskapp
+
+import android.app.Application
+import io.realm.Realm
+
+class TaskApp:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+    }
+}
